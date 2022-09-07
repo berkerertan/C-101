c08e3a8d437f911bed80ab5e0ab0aaffade57b7e
berkerertan <65434879+berkerertan@users.noreply.github.com>
2022-09-07 05:28:07 +0300

    Add files via upload

---
+++ if-else.cs
@@ -0,0 +1,28 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        int time = DateTime.Now.Hour;
+
+        if(time >=5 && time <11)
+            Console.WriteLine("günaydın");
+
+        else if(time <= 18)
+            Console.WriteLine("iyi günler");
+        
+        else
+            Console.WriteLine("iyi geceler");
+
+        string sonuc = time <=18 ? "iyi günler": "iyi geceler";
+            Console.WriteLine(sonuc);
+
+        string sonuc2 =time>=6 && time <11 ? "Günaydın":time <= 18 ? "iyi geceler": "iyi geceler";
+            Console.WriteLine(sonuc2);
+
+        
+
+
+
+
+    }
+}
