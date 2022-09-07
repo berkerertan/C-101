620feed65dee13b1f7f66813c2ebbbdadf00c1a0
berkerertan <65434879+berkerertan@users.noreply.github.com>
2022-09-07 04:27:19 +0300

    Add files via upload

---
+++ hatayönetimi.cs
@@ -0,0 +1,57 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // try
+        // {
+        //     Console.WriteLine("bir sayı gir");
+        //     int sayi = Convert.ToInt32(Console.ReadLine());
+        //     Console.WriteLine("girmis oldugunuz sayı şudur: " + sayi);
+        // }
+        // catch (Exception ex)
+        // {
+        //     Console.WriteLine("hata:" + ex.Message.ToString());
+
+        // }
+        // finally
+        // {
+        //     Console.WriteLine("işlem tamamlandı");
+
+        // }
+
+        // ERROR
+
+        try
+        {
+            //int a= int.Parse(null);
+            //int a= int.Parse("test");
+            int a= int.Parse("-20000000000");
+
+
+        }
+        catch (ArgumentNullException ex)
+        {
+            Console.WriteLine("bos deger girdiniz");
+            Console.WriteLine(ex);
+
+        }
+        catch (FormatException ex){
+            Console.WriteLine("veri tipi uygun değil");
+            Console.WriteLine(ex);
+
+
+        }
+        catch (OverflowException ex){
+            Console.WriteLine("cok kucuk veya cok buyuk bir değer girdiniz");
+            Console.WriteLine(ex);
+
+
+
+        }
+        finally{
+            Console.WriteLine("islem basarıyl tamamlandı");
+
+
+        }
+    }
+}
