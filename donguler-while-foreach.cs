2f7d8f66eeb1e4de284097c2c91597c3fde0965b
berkerertan <65434879+berkerertan@users.noreply.github.com>
2022-09-09 13:31:59 +0300

    Add files via upload

---
+++ donguler-while-foreach.cs
@@ -0,0 +1,41 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        //while
+        //1 den baslayarak konsoldan  girilen sayıya kadar ort. hesaplayıp konsola yazdırır.
+        
+        // Console.Write("Lutfen bır sayı girin:");
+
+        // int sayi = Convert.ToInt32(Console.ReadLine());
+        // int sayac =1;
+        // int toplam = 0;
+        // while (sayac<=sayi)
+        // {
+        //     toplam += sayac;
+        //     sayac ++;
+            
+        // }
+        
+        // Console.WriteLine(toplam/sayac);
+
+        // a' dan z'ye kadar olan tüm harfleri yazdır.
+        char character = 'a';
+
+
+        while (character<='z')
+        {
+            Console.Write(character);
+            character ++;
+        }
+
+        //foreach
+
+        string[] arabalar = {"BMW","MERCEDES","FORD","TOYOTA","NİSSAN"};
+
+        foreach (var araba in arabalar)
+        {
+            Console.WriteLine(araba);
+        }
+    }
+}
