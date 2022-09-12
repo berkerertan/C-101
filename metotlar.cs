69e026fb9e8007a9ec38af5717ec08fd9fe7c34f
berkerertan <65434879+berkerertan@users.noreply.github.com>
2022-09-12 04:19:59 +0300

    Add files via upload

---
+++ metotlar.cs
@@ -0,0 +1,45 @@
+﻿using System;
+namespace metotlar
+{
+    class Program
+    {
+     static void Main(string[] args)
+        {
+        // erisim_belirteci geri_donustipi metot_adi(parametreListesi/arguman)
+        // {
+        //     komutlar;
+        // }
+
+        int a = 2;
+        int b =3 ;
+        Console.WriteLine(a+b);
+
+        int sonuc=Topla(a,b);
+        Console.WriteLine(sonuc);
+        
+        Metotlar ornek = new Metotlar();
+        ornek.EkranaYazdir(Convert.ToString(sonuc));
+        int sonuc2 = ornek.ArttırveTopla(a,b);
+
+
+     }
+     static int Topla(int deger1,int deger2)
+     {
+        return(deger1+deger2);
+        }
+    }
+    class Metotlar
+    {
+     public void EkranaYazdir(string veri);
+        {
+        Console.WriteLine(veri);
+        }
+    public int ArttırveTopla(int deger1,int deger2)
+    {
+        deger1 += 1;
+        deger2 += 1;
+        return deger1+deger2;
+
+    }
+    }
+}
