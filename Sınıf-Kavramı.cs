7f786cee9389c38682be0a52a1c56c8cf878a025
Berker ERTAN <65434879+berkerertan@users.noreply.github.com>
2022-09-21 13:40:40 +0300

    Add files via upload

---
+++ Sınıf-Kavramı.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //erişim belirleyiciler public,private,internal,protected
+
+            Calisan calisan1 = new Calisan();
+            calisan1.ad="Ayşe";
+            calisan1.soyAd="kara";
+            calisan1.no=12345678;
+            calisan1.Departman="İnsan Kaynakları";
+
+            calisan1.CalisanBilgileri();
+
+            Calisan calisan2 = new Calisan();
+            calisan2.ad="Denis";
+            calisan2.soyAd="Arda";
+            calisan2.no=87654321;
+            calisan2.Departman="Satın-Alma";
+            calisan2.CalisanBilgileri();
+
+
+
+        }
+    }
+    class Calisan
+    {
+        public string ad;
+        public string soyAd;
+        public int no;
+        public string Departman;
+
+        public void CalisanBilgileri()
+        {
+            Console.WriteLine("Çalışan adı:{0}",ad);
+            Console.WriteLine("Çalışan adı:{0}",soyAd);
+            Console.WriteLine("Çalışan adı:{0}",no);
+            Console.WriteLine("Çalışan adı:{0}",Departman);
+
+
+        }
+
+
+
+    }
+}
