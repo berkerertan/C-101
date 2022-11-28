37398a5ef7f247b4d3c7ed665e7ec60d9f61e469
Berker ERTAN <65434879+berkerertan@users.noreply.github.com>
2022-11-28 11:43:32 +0300

    Add files via upload

---
+++ Fibonacci.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ortalama
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Dizinin derinliğini giriniz:");
+            int derinlik = Int32.Parse(Console.ReadLine());
+            int x=1,y=1,t=0;
+            
+
+            for (int i = 0; i < derinlik; i++)
+            {
+                Console.WriteLine(t);
+                t = x + y;
+                x=y;
+                y=t;
+            }
+
+            Console.WriteLine("Fibonacci dizisi= "+ (t));
+
+        }
+    }
+}
