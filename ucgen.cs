9ed08c1fe2aa1c9dbe78440c41a1d7608c5dee78
Berker ERTAN <65434879+berkerertan@users.noreply.github.com>
2022-12-04 14:19:33 +0300

    Add files via upload

---
+++ ucgen.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Eşkenar üçgenin kenar uzunluğunu giriniz: ");
+            int x = Int32.Parse(Console.ReadLine());
+            for (int i = 0; i < x; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
