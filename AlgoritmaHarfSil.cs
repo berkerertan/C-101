72f4b9f4e3321e5d186e156268a0511853b90c0c
Berker ERTAN <65434879+berkerertan@users.noreply.github.com>
2022-12-09 22:51:22 +0300

    Add files via upload

---
+++ AlgoritmaHarfSil.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Metini ve Sayıyı arasında ',' olacak şekilde girin: ");
+            string input1 = Console.ReadLine();
+            string[] words = input1.Split(",");
+            int sayi = Convert.ToInt32(words[1]);
+            Console.WriteLine(words[0].Remove(sayi,1));
+
+        }
+    }
+}
